01bd6cca157366425e8c0bbcbf221aa0b7ba78c7
Patryk Uran <trykpa@gmail.com>
2017-06-06 17:32:47 +0200

    shorten js function

--- js/app.js
+++ js/app.js
@@ -2,18 +2,7 @@ AOS.init({
   duration: 1200,
 });
 
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
-});
-
-$("li").click(function() {
+$("#toggle, li, #overlay").click(function() {
 	$('#toggle').toggleClass('active');
    	$('#overlay').toggleClass('open');
-});
-
-$("#overlay").click(function() {
-	$('#toggle').toggleClass('active');
-   	$('#overlay').toggleClass('open');
-	console.log("you clicked overlay");
 });
