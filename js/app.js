9f8ac0f35265cbb26de4cbde894975d8a166bd07
Patryk Uran <trykpa@gmail.com>
2017-06-06 17:53:10 +0200

    delete unnecessary files from libraries

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ AOS.init({
   duration: 1200,
 });
 
-$("#toggle, li, #overlay").click(function() {
+$("#toggle, #overlay").click(function() {
 	$('#toggle').toggleClass('active');
    	$('#overlay').toggleClass('open');
 });
