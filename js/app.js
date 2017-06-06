dd2e3b4939d86e0847e782c584b3f48f87a757fe
Patryk Uran <trykpa@gmail.com>
2017-06-06 13:28:49 +0200

    more code

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
 });
+
+$("li").click(function() {
+	$('#toggle').removeClass('active');
+	console.log("you clicked li");
+});
